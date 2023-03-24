61a590aa933aa5afa0a1e9dbebc290f34eeef775
leofuna <leo.funa@gmail.com>
2023-03-24 18:24:28 -0300

    template method pattern first studies

---
+++ modulo07/behavioral/03.template-method/src/business/base/baseBusiness.js
@@ -0,0 +1,23 @@
+import { NotImplementedException } from "../../util/exceptions"
+
+export default class BaseBusiness {
+  _validateRequiredFields(data) {
+    throw new NotImplementedException(this._validateRequiredFields.name)
+  }
+
+  _create(data) {
+    throw new NotImplementedException(this._create.name)
+  }
+  /*
+    Padrao do Martin Fowler
+    a proposta do padrao é garantir um fluxo de métodos, definindo uma sequencia a ser executada
+
+    Esse create é a implementaçao efetiva do Template Method
+  */
+  create(data) {
+    const isValid = this._validateRequiredFields(data)
+    if(!isValid) throw new Error('invalid data!')
+
+    return this._create(data)
+  }
+}
